ad414c45a517de912ef75dd7dd24dd9e66ee0918
David Nurdin <david.nurdin@respawnsive.com>
2025-10-06 00:59:47 +0200

    update

--- websocket-worker.php
+++ websocket-worker.php
@@ -9,9 +9,16 @@ $handler = static function (string $msg): array  {
     return ['message' => "Hello from PHP : " . $msg];
 };
 
+file_put_contents('php://stderr', "WebSocket worker started with PID " . getmypid() . "\n");
+
+
 $maxRequests = (int)($_SERVER['MAX_REQUESTS'] ?? 0);
+$maxRequests = 2 ;
+
 for ($nbRequests = 0; !$maxRequests || $nbRequests < $maxRequests; ++$nbRequests) {
+    file_put_contents('php://stderr', "Handled request #" . ($nbRequests + 1) . "\n");
     $keepRunning = \frankenphp_handle_request($handler);
+    file_put_contents('php://stderr', "Request handled, keepRunning=" . ($keepRunning ? 'true' : 'false') . "\n");
 
     // Call the garbage collector to reduce the chances of it being triggered in the middle of the handling of a request
     gc_collect_cycles();
@@ -20,3 +27,5 @@ for ($nbRequests = 0; !$maxRequests || $nbRequests < $maxRequests; ++$nbRequests
       break;
     }
 }
+
+file_put_contents('php://stderr', "Max requests reached, exiting\n");
