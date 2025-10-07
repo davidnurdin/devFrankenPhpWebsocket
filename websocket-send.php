e0e56d42a7a201ad43109ddd925915befcd9d661
David Nurdin <david.nurdin@respawnsive.com>
2025-10-07 13:20:32 +0200

    update

--- websocket-send.php
+++ websocket-send.php
@@ -1,7 +1,10 @@
 <?php
 
+echo "Get all WS clients ... <br>" ;
+
 foreach (frankenphp_ws_getClients() as $client)
 {
+    echo "Send HELLO to " . $client . "<br>" ;
     frankenphp_ws_send($client,"HELLO " . rand(1,1000));
 }
 
