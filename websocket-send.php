0e604e6b8e32c028b739cc8d78a7b18d4b2f8516
David Nurdin <david.nurdin@respawnsive.com>
2025-10-13 23:32:49 +0200

    ok route

--- websocket-send.php
+++ websocket-send.php
@@ -2,8 +2,9 @@
 
 echo "Get all WS clients ... <br>" ;
 
+var_dump(frankenphp_ws_listRoutes());
 
-foreach (frankenphp_ws_getClients() as $client)
+foreach (frankenphp_ws_getClients('/route2') as $client)
 {
     echo "Send HELLO to " . $client . "<br>" ;
     frankenphp_ws_send($client,"HELLO " . rand(1,1000));
